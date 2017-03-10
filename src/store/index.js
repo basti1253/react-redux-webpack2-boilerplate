90c130a25d5bd5f2c336a71028c481ef4af451af
basti <info@dynpages.de>
2017-03-10 13:51:46 +0100

    fix production build

--- src/store/index.js
+++ src/store/index.js
@@ -4,36 +4,30 @@ import thunk from 'redux-thunk';
 import immutableStateInvariant from 'redux-immutable-state-invariant';
 import logger from '../dev/logger';
 
-import rootReducer from '../reducers';
+import reducer from '../reducers';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 export default function configureStore() {
-  const middleWares = [
+  let middleWares = [
     thunk,
     promiseMiddleware,
     immutableStateInvariant()
   ];
   if (isProduction) {
-    const middleware = applyMiddleware(middleWares);
-
-    return createStore(
-      rootReducer,
-      middleware
-    );
+    return createStore(reducer, compose(applyMiddleware(...middleWares)));
   }
 
-  const middleware = applyMiddleware(...middleWares, logger);
-  const enhancer = compose(
-    middleware,
-    /* eslint-disable no-underscore-dangle */
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    /* eslint-enable */
-  );
+  middleWares = [...middleWares, logger];
 
   return createStore(
-    rootReducer,
-    enhancer
+    reducer,
+    compose(
+      applyMiddleware(...middleWares),
+      /* eslint-disable no-underscore-dangle */
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+      /* eslint-enable */
+    )
   );
 }
 
